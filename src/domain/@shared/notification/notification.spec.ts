eb5bcafe6b8f864c3d16a0400a1c7f69568262a7
Herlander Bento <herlanderbento19@gmail.com>
2023-03-06 21:51:23 +0100

    shared: create notification, notification-error, validator-interface

---
+++ src/domain/@shared/notification/notification.spec.ts
@@ -0,0 +1,55 @@
+import Notification from './notification';
+
+describe('Unit tests for notifications', () => {
+  it('should be able create errors', () => {
+    const notification = new Notification();
+
+    const error = {
+      message: 'error message',
+      context: 'userAdmin',      
+    };
+
+    notification.addError(error);
+
+    expect(notification.messages('userAdmin')).toBe(
+      'userAdmin: error message,'
+    );
+
+    const error2 = {
+      message: 'error message2',
+      context: 'teacher',
+    };
+
+    notification.addError(error2);
+
+    expect(notification.messages()).toBe(
+      'userAdmin: error message,teacher: error message2,'
+    );
+  });
+
+  it('should check if notification has at least one error', () => {
+    const notification = new Notification();
+
+    const error = {
+      message: 'error message',
+      context: 'userAdmin',
+    };
+
+    notification.addError(error);
+
+    expect(notification.hasErrors()).toBe(true);
+  });
+
+  it('should get all errors props', () => {
+    const notification = new Notification();
+
+    const error = {
+      message: 'error message',
+      context: 'teacher',
+    };
+
+    notification.addError(error);
+
+    expect(notification.getErrors()).toEqual([error]);
+  });
+});
