5eb9c06eb7b709a550ad51e9a6725f5a02a1d7bd
Herlander Bento <herlanderbento19@gmail.com>
2023-03-12 17:55:37 +0100

    student domain: create students address validator

---
+++ src/domain/students/validator/student-address.yup.validator.ts
@@ -0,0 +1,40 @@
+import * as yup from 'yup';
+import ValidatorInterface from '../../@shared/validator/validator.interface';
+import Address from '../value-object/address';
+
+export default class StudentAddressYupValidator
+  implements ValidatorInterface<Address>
+{
+  validate(entity: Address): void {
+    const { state, city, address } = entity;
+
+    try {
+      yup
+        .object()
+        .shape({
+          state: yup.string().required('State is required'),
+          city: yup.string().required('City is required'),
+          address: yup.string().required('Address is required'),
+        })
+        .validateSync(
+          {
+            state,
+            city,
+            address,
+          },
+          {
+            abortEarly: false,
+          }
+        );
+    } catch (errors) {
+      const err = errors as yup.ValidationError;
+
+      err.errors.forEach((error) => {
+        entity.notification.addError({
+          context: 'address',
+          message: error,
+        });
+      });
+    }
+  }
+}
