8aa269b3cac3412e82f54914f85e865e587d1696
Herlander Bento <herlanderbento19@gmail.com>
2023-03-14 17:51:26 +0100

    courses domain: create courses-repository-interface

---
+++ src/domain/courses/repository/courses-repository.interface.ts
@@ -0,0 +1,5 @@
+import RepositoryInterface from '../../@shared/repository/repository-interface';
+import Courses from '../entity/courses';
+
+export default interface CoursesRepositoryInterface
+  extends RepositoryInterface<Courses> {}
